96c72e1e116cb36da35d6de06636d2612eb5e3f4
s51360 <s51360@edu.campus02.at>
2023-02-05 18:08:13 +0100

    Initial commit

---
+++ src/test/java/org/campus02/ismani/BMICalculatorTest.java
@@ -0,0 +1,30 @@
+package org.campus02.ismani;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    private BMICalculator bmiCalculator;
+
+    @BeforeEach
+    void setUp() {
+        bmiCalculator = new BMICalculator(180, 1.80);
+    }
+
+    @Test
+    @DisplayName("Konstruktor Test")
+    void testConstructor() {
+        double expectedWeight = 180;
+        Assertions.assertEquals(expectedWeight, bmiCalculator.getWeightInKG());
+        Assertions.assertEquals(1.8, bmiCalculator.getHeightInMeters());
+    }
+
+    @Test
+    void reusult() {
+    }
+}
